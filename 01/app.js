141a99de4e60bcfb172dbee74f93c78959135614
gilbox <birmangil@gmail.com>
2015-01-19 23:19:48 -0600

    lesson 01

---
+++ 01/app.js
@@ -0,0 +1,11 @@
+var App = React.createClass({
+  render: function () {
+    return (
+      <div>
+        <h1 className='Title'>Leave a comment</h1>
+      </div>
+    )
+  }
+});
+
+React.render(<App />, document.getElementById('app'));
